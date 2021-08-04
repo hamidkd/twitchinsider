8b9d3b6a9251d670737b2ff0691b1213b0ca1667
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-08-04 18:37:01 -0400

    complete fetchAndSend

--- src/fetchAndSet.js
+++ src/fetchAndSet.js
@@ -6,10 +6,21 @@ const fetchAndSet = async ({ url, setter }) => {
     headers: {
       Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
       "Client-id": process.env.CLIENT_ID,
+      'accept': 'application/vnd.twitchtv.v5+json'
     },
   })
-    .then((res) => handleNetworkErrors(res))
-    .then((res) => console.log(res.json()));
+    .then(handleNetworkErrors)
+    .then((res) => {
+        console.log(res.json().data);
+        setter(res.json().data)
+    });
+};
+
+const handleNetworkErrors = (res) => {
+  if (!res.ok) {
+    alert(res.statusText);
+  }
+  return res;
 };
 
 export default fetchAndSet;
