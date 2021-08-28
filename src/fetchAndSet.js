c8d23932a70ae27c7371d83a56cfb175dd475374
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-08-27 22:24:51 -0400

    add cors HTTP request  header+ remove proxy server

--- src/fetchAndSet.js
+++ src/fetchAndSet.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 
 const fetchAndSet = async ({ url, setter }) => {
-  fetch("https://twitchinsider-proxyserver.herokuapp.com/proxyapi" + url, {
+  // fetch("https://twitchinsider-proxyserver.herokuapp.com/proxyapi" + url, {
+  //   method: "GET",
+  //   headers: {
+  //     Authorization: "Bearer drrnmb0fmdca9y7haqeam7cbltzywu",
+  //     "Client-id": "u7t2o9a71cdfrhtb4m7janstgmwymt",
+  //   },
+  // })
+  fetch("https://api.twitch.tv/helix" + url, {
     method: "GET",
     headers: {
-      Authorization: "Bearer drrnmb0fmdca9y7haqeam7cbltzywu",
-      "Client-id": "u7t2o9a71cdfrhtb4m7janstgmwymt",
+      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      "Client-id": `${process.env.CLIENT_ID}`,
+      accept: "application/vnd.twitchtv.v5+json",
     },
   })
     .then(handleNetworkErrors)
